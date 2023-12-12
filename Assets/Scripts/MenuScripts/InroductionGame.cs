2528d05f1f10f8df621f4627c8fad629acbf5b60
JhamilCastillo <jhamil.castillo@davinci.edu.ar>
2023-12-12 15:29:45 -0400

    Update InroductionGame.cs

--- Assets/Scripts/MenuScripts/InroductionGame.cs
+++ Assets/Scripts/MenuScripts/InroductionGame.cs
@@ -101,7 +101,38 @@ public class InroductionGame : MonoBehaviour
                     {
                         mainButtons[4].transform.DOScaleX(1, timerToAppear).SetEase(easeType);
 
+                        mainButtons[0].GetComponent<RectTransform>()
+                        .DOAnchorPos(mainButtons[0].GetComponent<RectTransform>().anchoredPosition + new Vector2(0, 80), timerToMove)
+                        .SetDelay(delayButtons)
+                        .SetEase(easeType);
+
                         EnableDisableButtons(true);
+                        mainButtons[1].transform.GetComponent<RectTransform>()
+                        .DOAnchorPos(mainButtons[1].GetComponent<RectTransform>().anchoredPosition + new Vector2(0, 80), timerToMove)
+                        .SetDelay(delayButtons)
+                        .SetEase(easeType);
+
+                        mainButtons[2].transform.GetComponent<RectTransform>()
+                        .DOAnchorPos(mainButtons[2].GetComponent<RectTransform>().anchoredPosition + new Vector2(0, 80), timerToMove)
+                        .SetDelay(delayButtons)
+                        .SetEase(easeType);
+
+                        mainButtons[3].transform.GetComponent<RectTransform>()
+                        .DOAnchorPos(mainButtons[3].GetComponent<RectTransform>().anchoredPosition + new Vector2(0, 80), timerToMove)
+                        .SetDelay(delayButtons)
+                        .SetEase(easeType);
+
+                        mainButtons[4].transform.GetComponent<RectTransform>()
+                        .DOAnchorPos(mainButtons[4].GetComponent<RectTransform>().anchoredPosition + new Vector2(0, 80), timerToMove)
+                        .SetDelay(delayButtons)
+                        .SetEase(easeType)
+
+                        .OnComplete(() =>
+                        {
+                            mainButtons[5].transform.DOScaleX(1, timerToAppear).SetEase(easeType);
+
+                            EnableDisableButtons(true);
+                        });
                     });
                 });
 
