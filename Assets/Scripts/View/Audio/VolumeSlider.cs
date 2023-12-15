8f52cb8256b85797b9741739d83291cb1dec838d
JhamilCastillo <jhamil.castillo@davinci.edu.ar>
2023-12-15 08:58:27 -0400

    Volumen persistente
    - Ahora cuando modificas el volumen desde opciones los valores se guardan en los player prefs
    - Ahora los valores tanto de los mixers como los sliders se cargan en el Start

--- Assets/Scripts/View/Audio/VolumeSlider.cs
+++ Assets/Scripts/View/Audio/VolumeSlider.cs
@@ -12,6 +12,17 @@ public class VolumeSlider : MonoBehaviour
         float volume = 20 * Mathf.Log10(_slider.value);
         volume = _slider.value == 0 ? -80 : volume;
 
+        PlayerPrefs.SetFloat(transform.gameObject.name + "Mixer", volume);
+        PlayerPrefs.SetFloat(transform.gameObject.name + "Slider", _slider.value);
+
         _master.SetFloat(name, volume);
+    }
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey(transform.gameObject.name + "Slider"))
+        {
+            _slider.value = PlayerPrefs.GetFloat(transform.gameObject.name + "Slider");
+        }
     }
 }
